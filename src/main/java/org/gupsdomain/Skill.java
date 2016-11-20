b6f5acacb55db4afbf2992a584646b8de34a67ec
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-11-20 21:35:11 +0100

    Provide a name for skill

--- src/main/java/org/gupsdomain/Skill.java
+++ src/main/java/org/gupsdomain/Skill.java
@@ -2,9 +2,12 @@ package org.gupsdomain;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Skill {
+    @XmlElement
+    private String name;
 }
